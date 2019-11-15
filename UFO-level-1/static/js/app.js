aae7c935a56c1f725a8ecc6b1a59c32dbf43bbd8
Pratikchhya <pratikchhyaa@gmail.com>
2019-11-14 21:24:33 -0500

    Aliens

---
+++ UFO-level-1/static/js/app.js
@@ -0,0 +1,22 @@
+// from data.js
+var tableData = data;
+console.log(tableData);
+
+var tbody = d3.select("tbody");
+console.log(data);
+
+// data.forEach(function(ufoSighting){
+//     console.log(ufoSighting);
+
+// });
+
+data.forEach(function(ufoSighting){
+    console.log(ufoSighting);
+    var row = tbody.append("tr");
+    Object.entries(ufoSighting).forEach(function([key,value]){
+        console.log(key,value);
+        var cell = tbody.append("td");
+        cell.text(value);
+    });
+});
+
